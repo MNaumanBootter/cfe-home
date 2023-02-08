bd7755935b8ab461d8c331f5c2ee145cffb56e81
Nauman Bootter <mnaumanbootter@gmail.com>
2023-02-08 20:31:36 +0500

    add product create and list endpoint

--- app/products/urls.py
+++ app/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
+    path("", views.ProductListCreateAPIView.as_view()),
     path("<int:pk>", views.ProductDetailAPIView.as_view()),
 ]
--- app/products/views.py
+++ app/products/views.py
@@ -3,7 +3,17 @@ from products.models import Product
 from products.serializers import ProductSerializer
 
 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer: ProductSerializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content")
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
