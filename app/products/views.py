a8a97732b017eb6e12ade66a85bf8494f1208ff8
Nauman Bootter <mnaumanbootter@gmail.com>
2023-02-09 15:04:08 +0500

    add alternate function based views for product
    - add alternate function based views for product create and read operations

--- app/products/views.py
+++ app/products/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -17,3 +20,29 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+@api_view(["POST", "GET"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj).data
+            return Response(data)
+
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content")
+            if content is None:
+                content = title
+            serializer.save(content=content)
+
+        return Response(serializer.data)
