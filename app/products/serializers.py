095bcb175a247d4849ff8d86667126229f5716b9
Nauman Bootter <mnaumanbootter@gmail.com>
2023-02-28 14:34:13 +0500

    add create and update serializer functions for eg

--- app/products/serializers.py
+++ app/products/serializers.py
@@ -6,6 +6,7 @@ class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
@@ -17,7 +18,17 @@ class ProductSerializer(serializers.ModelSerializer):
             "discount",
             "url",
             "edit_url",
+            "email",
         ]
+
+    def create(self, validated_data):
+        email = validated_data.pop("email")
+        obj = super().create(validated_data)
+        return obj
+
+    def update(self, instance, validated_data):
+        email = validated_data.pop("email")
+        return super().update(instance, validated_data)
 
     def get_edit_url(self, obj):
         request = self.context.get("request")
