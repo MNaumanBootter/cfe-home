ee05c78a38773f16a71561cff4fbbdc73873d5d9
Nauman Bootter <mnaumanbootter@gmail.com>
2023-02-28 13:26:59 +0500

    add 2 new fields in product serializer

--- app/products/serializers.py
+++ app/products/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    discount = serializers.SerializerMethodField()
+    discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
     class Meta:
         model = Product
         fields = [
@@ -13,7 +15,15 @@ class ProductSerializer(serializers.ModelSerializer):
             "price",
             "sale_price",
             "discount",
+            "url",
+            "edit_url",
         ]
+
+    def get_edit_url(self, obj):
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_discount(self, obj):
         if not hasattr(obj, "id"):
--- app/products/urls.py
+++ app/products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path("", views.ProductListCreateAPIView.as_view()),
-    path("<int:pk>/", views.ProductDetailAPIView.as_view()),
-    path("<int:pk>/update", views.ProductUpdateAPIView.as_view()),
+    path("", views.ProductListCreateAPIView.as_view(), name="product-list"),
+    path("<int:pk>/", views.ProductDetailAPIView.as_view(), name="product-detail"),
+    path("<int:pk>/update", views.ProductUpdateAPIView.as_view(), name="product-edit"),
     path("<int:pk>/delete", views.ProductDestroyAPIView.as_view()),
 ]
