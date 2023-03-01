ccb43fe9c21f0aa47ef429b13d43d8ee59b3388e
Nauman Bootter <mnaumanbootter@gmail.com>
2023-03-01 14:42:08 +0500

    add product title validators

--- app/products/serializers.py
+++ app/products/serializers.py
@@ -1,16 +1,20 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from products.models import Product
+from products.validators import unique_field_validator, validate_title
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
+    title = serializers.CharField(validators=[unique_field_validator, validate_title])
+    name = serializers.CharField(source="title")
     class Meta:
         model = Product
         fields = [
             "id",
             "title",
+            "name",
             "content",
             "price",
             "sale_price",
---
+++ app/products/validators.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from products.models import Product
+from rest_framework.validators import UniqueValidator
+
+def validate_title(value: str):
+    if "hello" in value.lower():
+        raise serializers.ValidationError(f"'hello' is not allowed for product title")
+    return value
+
+unique_field_validator = UniqueValidator(queryset=Product.objects.all())
