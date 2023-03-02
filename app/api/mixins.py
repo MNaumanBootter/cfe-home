b83fe06c824196bef3dbb9f72ecc0f9ea2a32e75
Nauman Bootter <mnaumanbootter@gmail.com>
2023-03-02 12:04:28 +0500

    add code to allow staff users all products access

--- app/api/mixins.py
+++ app/api/mixins.py
@@ -8,9 +8,14 @@ class StaffEditorPermissionMixin():
 
 class UserQuerySetMixin():
     user_field = 'user'
+    allow_staff_view = False
+
     def get_queryset(self, *args, **kwargs):
+        user = self.request.user
         lookup_data = {}
-        lookup_data[self.user_field] = self.request.user
-        print(lookup_data)
+        lookup_data[self.user_field] = user
         qs = super().get_queryset(*args, **kwargs)
+
+        if self.allow_staff_view and user.is_staff:
+            return qs
         return qs.filter(**lookup_data)
